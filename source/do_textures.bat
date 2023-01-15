82e5684f393d64d51beb1d4488de9bd95b103756
Spear <rlopezll@gmail.com>
2023-01-15 03:56:49 +0100

    remaping textures ground

--- source/do_textures.bat
+++ source/do_textures.bat
@@ -1,9 +1,5 @@
 ..\tools\BMP2TIM\BMP2TIM.exe textures\Path_Texture.bmp data\Path_Texture.tim 4 -clut=0,480 -org=640,0
-
-..\tools\BMP2TIM\BMP2TIM.exe textures\Scanners_Texture.bmp data\Scanners_Texture.tim 4 -clut=80,480 -org=704,0
-..\tools\BMP2TIM\BMP2TIM.exe textures\Game_Ground_P1.bmp data\Game_Ground_P1.tim 4 -clut=96,480 -org=704,64
-..\tools\BMP2TIM\BMP2TIM.exe textures\Game_Ground_P2.bmp data\Game_Ground_P2.tim 4 -clut=112,480 -org=704,128
-rem ..\tools\BMP2TIM\BMP2TIM.exe textures\Scanners_Texture.bmp data\Scanners_Texture.tim 4 -clut=128,480 -org=704,192
+..\tools\BMP2TIM\BMP2TIM.exe textures\Scanners_Texture.bmp data\Scanners_Texture.tim 4 -clut=16,480 -org=704,0
 
 ..\tools\BMP2TIM\BMP2TIM.exe textures\CamisetaRoja.bmp data\CamisetaRoja.tim 4 -clut=16,481 -org=768,0
 ..\tools\BMP2TIM\BMP2TIM.exe textures\CamisetaAzul.bmp data\CamisetaAzul.tim 4 -clut=32,481 -org=832,0
@@ -25,3 +21,6 @@ rem ..\tools\BMP2TIM\BMP2TIM.exe textures\Scanners_Texture.bmp data\Scanners_Tex
 
 ..\tools\BMP2TIM\BMP2TIM.exe textures\DESCONTROL_PressStart.bmp data\DESCONTROL.tim 8 -clut=0,480 -org=640,0
 ..\tools\BMP2TIM\BMP2TIM.exe textures\GameOver.bmp data\GameOver.tim 8 -clut=0,480 -org=640,0 -mpink
+
+..\tools\BMP2TIM\BMP2TIM.exe textures\Game_Ground_P1.bmp data\Game_Ground_P1.tim 8 -clut=0,483 -org=640,256
+..\tools\BMP2TIM\BMP2TIM.exe textures\Game_Ground_P2.bmp data\Game_Ground_P2.tim 8 -clut=256,483 -org=768,256
